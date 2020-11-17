a19bcc79bb85d35e3551d466d2e7eab58a0bea92
chenhao <cs_chenhao@mail.scut.edu.cn>
2020-11-17 20:42:19 +0800

    修复exp3的bug

--- experiment/experiment3.py
+++ experiment/experiment3.py
@@ -89,5 +89,5 @@ class Experiment3:
 
 if __name__ == '__main__':
     gpuutils.which_gpu_to_use(gpu_index=2)
-    exp = Experiment3(symbol_win_size=15)
+    exp = Experiment3(symbol_win_size=11)
     exp.start_train_task()
