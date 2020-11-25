475e6c23ab3a58f16ee37c0a2c7adf90ead0ecab
chenhao <cs_chenhao@mail.scut.edu.cn>
2020-11-25 21:19:38 +0800

    实现基于regressionNN, win-size19

--- experiment/evaluation.py
+++ experiment/evaluation.py
@@ -39,7 +39,7 @@ class Evaluation3:
     """
     def __init__(self, weight_filename):
         # win-size
-        win_sz = 15
+        win_sz = 7
         # model
         self.model = Experiment3.build_cleaner(win_size=16 * (win_sz - 1))
 
@@ -64,7 +64,7 @@ class Evaluation2:
     """
     def __init__(self, weight_filename):
         # win size
-        win_sz = 7
+        win_sz = 19
 
         # model to evaluate
         self.model = Experiment2.build_nn(win_sz)
@@ -84,5 +84,5 @@ class Evaluation2:
 
 if __name__ == '__main__':
     #Evaluation1('cleaner_final.h5').eval()
-    # Evaluation2('cleaner_final.h5').eval()
-    Evaluation3('cleaner_final.h5').eval()
+    Evaluation2('cleaner_final.h5').eval()
+    # Evaluation3('cleaner_final.h5').eval()
