935f52a57ddd12c3f031382579977df8e86cde9b
chenhao <cs_chenhao@mail.scut.edu.cn>
2020-11-25 21:31:57 +0800

    实现基于regressionNN, win-size19

--- experiment/experiment3.py
+++ experiment/experiment3.py
@@ -89,5 +89,5 @@ class Experiment3:
 
 if __name__ == '__main__':
     gpuutils.which_gpu_to_use(gpu_index=1)
-    exp = Experiment3(symbol_win_size=19)
+    exp = Experiment3(symbol_win_size=23)
     exp.start_train_task()
