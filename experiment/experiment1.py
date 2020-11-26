4f55ecb0a1e3ecfea9de6afc2b50517a91078e80
chenhao <cs_chenhao@mail.scut.edu.cn>
2020-11-26 19:56:07 +0800

    限制训练长度3w

--- experiment/experiment1.py
+++ experiment/experiment1.py
@@ -264,7 +264,7 @@ class Experiment1:
 
 
 if __name__ == '__main__':
-    gpuutils.which_gpu_to_use(gpu_index=0)
+    gpuutils.which_gpu_to_use(gpu_index=1)
     exp = Experiment1(symbol_win_size=19)
     exp.print_experiment_context()
     exp.start_train_task()
